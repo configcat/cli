db5e9a60e794d88817b17c55a11de656c73307c9
Peter Csajtai <peter.csajtai@outlook.com>
2021-04-28 22:21:39 +0200

    Update Cat.cs

--- src/ConfigCat.Cli/Commands/Cat.cs
+++ src/ConfigCat.Cli/Commands/Cat.cs
@@ -1,7 +1,5 @@
 ﻿using ConfigCat.Cli.Services;
 using ConfigCat.Cli.Services.Rendering;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConfigCat.Cli.Commands
@@ -15,56 +13,49 @@ namespace ConfigCat.Cli.Commands
             this.output = output;
         }
 
-        public string Name => "whoisthebestcat";
-
-        public string Description => "Well, who?";
-
-        public IEnumerable<string> Aliases => new[] { "cat" };
-
-        public Task<int> InvokeAsync(CancellationToken token)
+        public Task<int> InvokeAsync()
         {
-            this.output.Write(@"
-                                                              oK        
-                                                             koK     
-                                                           Xk;oK
-                                                         Xkl;:oK
-      No                                               Xkl;;;:oK
-      N0X                                            Xkl;;;;;:oK
-      Ko;xK                                        Xkl;;;;;;;:oK
-      Ko;;cxK                                    Xkl;;;;;;;;;:oK
-      Kl;;;;cxK                                Xkl;;;;;;;;;;;:oK
-      Kl;;;;;;cxK                           Xkdl;;;;;;;;;;;;;:oK
-      Kl;;;;;;;;cxK                       Xkl;;;;;;;;;;;;;;;;:oK
-      Kl;;;;;;;;;;cxK                   Xkl;;;;;;;;;;;;;;;;;;:oK
-      Kl;;;;;;;;;;;;cdxxxxxxxxxxxxxxxxxdl;;;;;;;;;;;;;;;;;;;;:lK
-      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK
-      Kl;;;;;;;;;;;;:::::::::::::::::::::::::::::;;;;;;;;;;;;:lK
-      Kl;;;;;;;;cdkO000000000KK00000KK000K00000000Oxl:;;;;;;;:lK                www
-      Kl;;;;;;cxXNNXOkk0kkOKNWMMMMMMMMMN0dc ``` okXWNOo;;;;;;:lK             N0kddodxOX
-      Kl;;;;;cOWWKxok0KXK0kxx0WMMMMMMW0            xNWKo;;;;;:lK           NOl:;;;;;;;cxX
-      Kl;;;;;dNWKoxXWMMMMMMNkoOWMMMMMO              dNWOc;;;;:lK          Nk:;;;;;;;;;;;oK
-      Kl;;;;;xNNxoKWMMMMMMMMNxdXMMMMWl               KW0c;;;;:lK          Kl;;;;;;;;;;;;:dN
-      Kl;;;;;xNNkoKWMMMMMMMMXddXMMMMWo               KW0c;;;;:lK          Ko;;;;;;;;;;;;;c0
-      Kl;;;;;dXWKddKWMMMMMWXxo0WMMMMMK              kWWk:;;;;:lK           k:;;;;;;;;;;;;:xN
-      Kl;;;;;:xNWXkoxO000OkokKWMMMMMMMXd          c0WW0l;;;;;:lK           Xo;;;;;;;;;;;;;oX
-      Kl;;;;;;:oOXNX0Ok0kk0XWMMMMMMMMMMWNOdollodkXNNKxc;;;;;;:lK            k:;;;;;;;;;;;;oX
-      Kl;;;;;;;;:loxkkkkkkkkkkkkkkkkkkkkkkkkkkkkkxdoc;;;;;;;;:lK            0c;;;;;;;;;;;;oX
-      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK            0c;;;;;;;;;;;;dN
-      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK            Oc;;;;;;;;;;;:kk
-  oOdddddddddddddc:;;;;;;;;;;;dd;;;;;bb;;;;;;;;;;:cdddddddddddddddddOo     Nx:;;;;;;;;;;;ln
-      NOxxxxxxxx:;;;;;;;;;;;;;;;dO;Ob;;;;;;;;;;;;;;;:xxxxxxxxxKK           Kl;;;;;;;;;;;:kN
-oOdddddddddddddddddddoc:;;;;;;;;;;0;;;;;;;;;;:clddddddddddddddddddddddOo  Nx:;;;;;;;;;;:xN
-       Xkxxxxxx:;;;;;;;;;;;;;;;;;;0;;;;;;;;;;;;;;;;;;:xxxxxxxxKK         Nk:;;;;;;;;;;:dX
-  oOddddddoooooooooc:;;;;;;;;;;;;;o;;;;;;;;;;;;;;:ccccccccccclddddddOo  Xx:;;;;;;;;;;:xX
-        Xd;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK       0o:;;;;;;;;;;lON
-         Xd:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK     Kxc;;;;;;;;;;cxK 
-          NOl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK  N0dc;;;;;;;;;;cd0N
-            Xkl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:xX0xo:;;;;;;;;;:lxK 
-              XOoc;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;cc:;;;;;;;;cox0N 
-                 KOxoc:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;cloxOKN
-                      cccdxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxbl
-
-");
+            this.output.WriteLine();
+            this.output.WriteLine("                                                              oK");        
+            this.output.WriteLine("                                                             koK");     
+            this.output.WriteLine("                                                           Xk;oK");
+            this.output.WriteLine("                                                         Xkl;:oK");
+            this.output.WriteLine("      No                                               Xkl;;;:oK");
+            this.output.WriteLine("      N0X                                            Xkl;;;;;:oK");
+            this.output.WriteLine("      Ko;xK                                        Xkl;;;;;;;:oK");
+            this.output.WriteLine("      Ko;;cxK                                    Xkl;;;;;;;;;:oK");
+            this.output.WriteLine("      Kl;;;;cxK                                Xkl;;;;;;;;;;;:oK");
+            this.output.WriteLine("      Kl;;;;;;cxK                           Xkdl;;;;;;;;;;;;;:oK");
+            this.output.WriteLine("      Kl;;;;;;;;cxK                       Xkl;;;;;;;;;;;;;;;;:oK");
+            this.output.WriteLine("      Kl;;;;;;;;;;cxK                   Xkl;;;;;;;;;;;;;;;;;;:oK");
+            this.output.WriteLine("      Kl;;;;;;;;;;;;cdxxxxxxxxxxxxxxxxxdl;;;;;;;;;;;;;;;;;;;;:lK");
+            this.output.WriteLine("      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK");
+            this.output.WriteLine("      Kl;;;;;;;;;;;;:::::::::::::::::::::::::::::;;;;;;;;;;;;:lK");
+            this.output.WriteLine("      Kl;;;;;;;;cdkO000000000KK00000KK000K00000000Oxl:;;;;;;;:lK                www");
+            this.output.WriteLine("      Kl;;;;;;cxXNNXOkk0kkOKNWMMMMMMMMMN0dc ``` okXWNOo;;;;;;:lK             N0kddodxOX");
+            this.output.WriteLine("      Kl;;;;;cOWWKxok0KXK0kxx0WMMMMMMW0            xNWKo;;;;;:lK           NOl:;;;;;;;cxX");
+            this.output.WriteLine("      Kl;;;;;dNWKoxXWMMMMMMNkoOWMMMMMO              dNWOc;;;;:lK          Nk:;;;;;;;;;;;oK");
+            this.output.WriteLine("      Kl;;;;;xNNxoKWMMMMMMMMNxdXMMMMWl               KW0c;;;;:lK          Kl;;;;;;;;;;;;:dN");
+            this.output.WriteLine("      Kl;;;;;xNNkoKWMMMMMMMMXddXMMMMWo               KW0c;;;;:lK          Ko;;;;;;;;;;;;;c0");
+            this.output.WriteLine("      Kl;;;;;dXWKddKWMMMMMWXxo0WMMMMMK              kWWk:;;;;:lK           k:;;;;;;;;;;;;:xN");
+            this.output.WriteLine("      Kl;;;;;:xNWXkoxO000OkokKWMMMMMMMXd          c0WW0l;;;;;:lK           Xo;;;;;;;;;;;;;oX");
+            this.output.WriteLine("      Kl;;;;;;:oOXNX0Ok0kk0XWMMMMMMMMMMWNOdollodkXNNKxc;;;;;;:lK            k:;;;;;;;;;;;;oX");
+            this.output.WriteLine("      Kl;;;;;;;;:loxkkkkkkkkkkkkkkkkkkkkkkkkkkkkkxdoc;;;;;;;;:lK            0c;;;;;;;;;;;;oX");
+            this.output.WriteLine("      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK            0c;;;;;;;;;;;;dN");
+            this.output.WriteLine("      Kl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:lK            Oc;;;;;;;;;;;:kk");
+            this.output.WriteLine("  oOdddddddddddddc:;;;;;;;;;;;dd;;;;;bb;;;;;;;;;;:cdddddddddddddddddOo     Nx:;;;;;;;;;;;ln");
+            this.output.WriteLine("      NOxxxxxxxx:;;;;;;;;;;;;;;;dO;Ob;;;;;;;;;;;;;;;:xxxxxxxxxKK           Kl;;;;;;;;;;;:kN");
+            this.output.WriteLine("oOdddddddddddddddddddoc:;;;;;;;;;;0;;;;;;;;;;:clddddddddddddddddddddddOo  Nx:;;;;;;;;;;:xN");
+            this.output.WriteLine("       Xkxxxxxx:;;;;;;;;;;;;;;;;;;0;;;;;;;;;;;;;;;;;;:xxxxxxxxKK         Nk:;;;;;;;;;;:dX");
+            this.output.WriteLine("  oOddddddoooooooooc:;;;;;;;;;;;;;o;;;;;;;;;;;;;;:ccccccccccclddddddOo  Xx:;;;;;;;;;;:xX");
+            this.output.WriteLine("        Xd;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK       0o:;;;;;;;;;;lON");
+            this.output.WriteLine("         Xd:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK     Kxc;;;;;;;;;;cxK");
+            this.output.WriteLine("          NOl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:kK  N0dc;;;;;;;;;;cd0N");
+            this.output.WriteLine("            Xkl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:xX0xo:;;;;;;;;;:lxK");
+            this.output.WriteLine("              XOoc;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;cc:;;;;;;;;cox0N");
+            this.output.WriteLine("                 KOxoc:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;cloxOKN");
+            this.output.WriteLine("                      cccdxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxbl");
+            this.output.WriteLine();
 
             return Task.FromResult(ExitCodes.Ok);
         }
