ec9766885d3b6edac1b1162b8ac007f31f4707fc
Peter Csajtai <peter.csajtai@outlook.com>
2025-03-04 20:05:50 +0100

    Update IgnoreTests.cs

--- test/ConfigCat.Cli.Tests/IgnoreTests.cs
+++ test/ConfigCat.Cli.Tests/IgnoreTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,11 +14,11 @@ namespace ConfigCat.Cli.Tests;
 public class IgnoreTests
 {
     [Theory]
-    [InlineData("a", new[] { @"a\.ignore", @"a\b\.ignore", @"a\c\f\.ignore" }, 8)]
-    [InlineData(@"a\b", new[] { @"a\.ignore", @"a\b\.ignore" }, 4)]
-    [InlineData(@"a\c", new[] { @"a\.ignore", @"a\c\f\.ignore" }, 3)]
-    [InlineData(@"a\c\f", new[] { @"a\.ignore", @"a\c\f\.ignore" }, 1)]
-    [InlineData(@"a\c\g", new[] { @"a\.ignore" }, 1)]
+    [InlineData("a", new[] { "a/.ignore", "a/b/.ignore", "a/c/f/.ignore" }, 8)]
+    [InlineData("a/b", new[] { "a/.ignore", "a/b/.ignore" }, 4)]
+    [InlineData("a/c", new[] { "a/.ignore", "a/c/f/.ignore" }, 3)]
+    [InlineData("a/c/f", new[] { "a/.ignore", "a/c/f/.ignore" }, 1)]
+    [InlineData("a/c/g", new[] { "a/.ignore" }, 1)]
     public async Task Test_IgnoreFile_Collect(string searchDir, string[] expectedIgnoreFiles, int fileCount)
     {
         var executingDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), searchDir));
@@ -25,21 +26,21 @@ public class IgnoreTests
         var repoDir = await client.GetRepoRootDirectoryOrNull(executingDir);
         var (ignoreFiles, files) = FileCollector.GetFilesFromWorkSpace(executingDir, repoDir);
 
-        var expectations = expectedIgnoreFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f))
-            .Concat([Path.Combine(repoDir.FullName, ".gitignore")]).Order().ToArray();
+        var expectations = expectedIgnoreFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f).WithSlashes())
+            .Concat([Path.Combine(repoDir.FullName, ".gitignore").WithSlashes()]).Order().ToArray();
 
-        var ignoreFileNames = ignoreFiles.Select(f => f.FullName).Order();
+        var ignoreFileNames = ignoreFiles.Select(f => f.FullName.WithSlashes()).Order();
 
         Assert.Equal(expectations, ignoreFileNames);
         Assert.Equal(fileCount, files.Count());
     }
 
     [Theory]
-    [InlineData("a", new[] { @"a\b\d\d.txt", @"a\b\d\h.txt", @"a\b\e\e.txt" })]
-    [InlineData(@"a\b", new[] { @"a\b\d\d.txt", @"a\b\d\h.txt", @"a\b\e\e.txt" })]
-    [InlineData(@"a\b\d", new[] { @"a\b\d\d.txt", @"a\b\d\h.txt" })]
-    [InlineData(@"a\b\e", new[] { @"a\b\e\e.txt" })]
-    [InlineData(@"a\c", new string[0])]
+    [InlineData("a", new[] { "a/b/d/d.txt", "a/b/d/h.txt", "a/b/e/e.txt" })]
+    [InlineData("a/b", new[] { "a/b/d/d.txt", "a/b/d/h.txt", "a/b/e/e.txt" })]
+    [InlineData("a/b/d", new[] { "a/b/d/d.txt", "a/b/d/h.txt" })]
+    [InlineData("a/b/e", new[] { "a/b/e/e.txt" })]
+    [InlineData("a/c", new string[0])]
     public async Task Test_IgnoreFile_Process_With_GitIgnore(string searchDir, string[] expectedFiles)
     {
         var executingDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), searchDir));
@@ -48,21 +49,21 @@ public class IgnoreTests
         var fileCollector = new FileCollector(new Output(new CliOptions()));
         var files = (await fileCollector.CollectAsync(executingDir, repoDir, CancellationToken.None)).ToArray();
 
-        var expectations = expectedFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f)).Order()
+        var expectations = expectedFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f).WithSlashes()).Order()
             .ToArray();
-        var fileNames = files.Select(f => f.FullName).Order();
+        var fileNames = files.Select(f => f.FullName.WithSlashes()).Order();
 
         Assert.Equal(expectations, fileNames);
     }
 
     [Theory]
     [InlineData("a",
-        new[] { @"a\a.txt", @"a\b\d\d.txt", @"a\b\d\h.txt", @"a\b\e\e.txt", @"a\c\c.txt", @"a\c\g\g.txt" })]
-    [InlineData(@"a\b", new[] { @"a\b\d\d.txt", @"a\b\d\h.txt", @"a\b\e\e.txt" })]
-    [InlineData(@"a\b\d", new[] { @"a\b\d\d.txt", @"a\b\d\h.txt" })]
-    [InlineData(@"a\b\e", new[] { @"a\b\e\e.txt" })]
-    [InlineData(@"a\c", new[] { @"a\c\c.txt", @"a\c\g\g.txt" })]
-    [InlineData(@"a\c\g", new[] { @"a\c\g\g.txt" })]
+        new[] { "a/a.txt", "a/b/d/d.txt", "a/b/d/h.txt", "a/b/e/e.txt", "a/c/c.txt", "a/c/g/g.txt" })]
+    [InlineData("a/b", new[] { "a/b/d/d.txt", "a/b/d/h.txt", "a/b/e/e.txt" })]
+    [InlineData("a/b/d", new[] { "a/b/d/d.txt", "a/b/d/h.txt" })]
+    [InlineData("a/b/e", new[] { "a/b/e/e.txt" })]
+    [InlineData("a/c", new[] { "a/c/c.txt", "a/c/g/g.txt" })]
+    [InlineData("a/c/g", new[] { "a/c/g/g.txt" })]
     public async Task Test_IgnoreFile_Process_Without_GitIgnore(string searchDir, string[] expectedFiles)
     {
         var executingDir = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), searchDir));
@@ -70,9 +71,9 @@ public class IgnoreTests
         var fileCollector = new FileCollector(new Output(new CliOptions()));
         var files = (await fileCollector.CollectAsync(executingDir, repoDir, CancellationToken.None)).ToArray();
 
-        var expectations = expectedFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f)).Order()
+        var expectations = expectedFiles.Select(f => Path.Combine(Directory.GetCurrentDirectory(), f).WithSlashes()).Order()
             .ToArray();
-        var fileNames = files.Select(f => f.FullName).Order();
+        var fileNames = files.Select(f => f.FullName.WithSlashes()).Order();
 
         Assert.Equal(expectations, fileNames);
     }
